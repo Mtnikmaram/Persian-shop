8752955baf610a51ea3575d1d19624b34a78cc2c
Mtnikmaram <mtnikmaram@gmail.com>
2023-12-01 22:58:37 +0330

    adding the secret key

--- persianshop/settings.py
+++ persianshop/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gatn!$p25vw&)ys7b%*z=!g+nxoclm%7^149i!=9nd$yru%a-0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['https://persian-shop-04122018a8ef.herokuapp.com/','localhost']
-
 
 # Application definition
 
