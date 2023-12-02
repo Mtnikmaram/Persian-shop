4086ad1ce7836d50f52bd51b2ca293045777f419
Mtnikmaram <mtnikmaram@gmail.com>
2023-12-02 23:11:53 +0330

    editing the setting file

--- persianshop/settings.py
+++ persianshop/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = "django-insecure-gatn!$p25vw&)ys7b%*z=!g+nxoclm%7^149i!=9nd$yru%a-0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-X_FRAME_OPTIONS = 'SAMEORIGIN'
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+DEBUG = 'DEVELOPMENT' in os.environ
+
 ALLOWED_HOSTS = ['persian-shop-04122018a8ef.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
