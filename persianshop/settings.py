db0e8bd16275e2e2c200745656f7c2d95d5d03c2
Mtnikmaram <mtnikmaram@gmail.com>
2023-12-06 22:55:16 +0330

    deploying to heroku

--- persianshop/settings.py
+++ persianshop/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'p)cmf-(a(fxl=lbx)ygnkz5s)m4j1!&psn0i_b9df#^h*gs)i)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DJANGO_ENV') == 'development'
-
+DEBUG = False
 ALLOWED_HOSTS = ['persian-shop-04122018a8ef.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
@@ -173,11 +172,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
