790a2547ba118234c2f8e1c8a478caa5a33476b3
Mtnikmaram <mtnikmaram@gmail.com>
2023-12-07 14:55:14 +0330

    finishing up

--- persianshop/settings.py
+++ persianshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p)cmf-(a(fxl=lbx)ygnkz5s)m4j1!&psn0i_b9df#^h*gs)i)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['persian-shop-04122018a8ef.herokuapp.com', 'localhost', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
