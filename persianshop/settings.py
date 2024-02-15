5e3c9108cae28804da615fed6047f9336be514df
Mtnikmaram <mtnikmaram@gmail.com>
2024-02-15 21:32:33 +0330

    editing the setting.py

--- persianshop/settings.py
+++ persianshop/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    #'allauth.account.middleware.AccountMiddleware',
 
 ]
 
