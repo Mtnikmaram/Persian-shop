b4342bd85d959ed700e540689aafd3d482f59708
Mtnikmaram <mtnikmaram@gmail.com>
2024-02-15 12:51:36 +0330

    DEBUG = False

--- persianshop/settings.py
+++ persianshop/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 
 ]
 
