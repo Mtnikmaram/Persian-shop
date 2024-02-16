7ae7fea6b4b0a0fdb0b8de7134b6eea8e1d26fd1
Mtnikmaram <mtnikmaram@gmail.com>
2024-02-17 00:36:41 +0330

    styling the 404 page

--- persianshop/settings.py
+++ persianshop/settings.py
@@ -64,8 +64,10 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'allauth.account.middleware.AccountMiddleware',
+
     
 ]
 
