091346b77f4078981268db03e7066e1d9aa3409b
Mtnikmaram <mtnikmaram@gmail.com>
2024-02-18 00:13:36 +0330

    adding product pictures

--- persianshop/settings.py
+++ persianshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = ['https://persian-shop.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['persian-shop.onrender.com', 'localhost', '127.0.0.1']
 
 #RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 #if RENDER_EXTERNAL_HOSTNAME:
@@ -184,6 +184,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 	
 MEDIA_URL = '/media/'	
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'	
 STATIC_URL = '/static/'	
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'	
