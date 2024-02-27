198de03f5fcf0185e1435a05391fcff9a3d52591
Mtnikmaram <mtnikmaram@gmail.com>
2024-02-27 22:45:23 +0330

    making the blog app

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+
